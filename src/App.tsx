743b4d5af7816e9ee1a212ee0b8acf19701fe8da
Shirley Park <shirley.park7@gmail.com>
2023-07-30 22:43:27 +1200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg'
+import { ProductPage } from './ProductPage'
+
+function App() {
+  return (
+    <div className="App">
+      <ProductPage />
+    </div>
+  )
+}
+
+export default App
